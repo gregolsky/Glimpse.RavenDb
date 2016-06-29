6170e74a352ecba4ba1ebda198b47ae03a058ad5
Grzegorz Lachowski <gregolsky@ravendb.net>
2016-06-29 12:00:06 +0200

    bump version and indicate it's custom build

--- Glimpse.RavenDb/Properties/AssemblyInfo.cs
+++ Glimpse.RavenDb/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Glimpse.RavenDb")]
-[assembly: AssemblyDescription("RavenDb plugin for Glimpse")]
+[assembly: AssemblyDescription("RavenDb plugin for Glimpse - custom Hibernating Rhinos build")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Glimpse.RavenDb")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.1.0.0")]
+[assembly: AssemblyFileVersion("3.1.0.0")]
